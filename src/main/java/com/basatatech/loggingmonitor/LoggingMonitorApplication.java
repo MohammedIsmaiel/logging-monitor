6d934c5a87e358bcf0c847347c3163ea2961a954
Mohammed-Ismaiel <mohammedismaiel333.z@gmail.com>
2024-01-15 11:06:31 +0200

    init commit

---
+++ src/main/java/com/basatatech/loggingmonitor/LoggingMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.basatatech.loggingmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggingMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoggingMonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/basatatech/loggingmonitor/LoggingMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basatatech.loggingmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggingMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
