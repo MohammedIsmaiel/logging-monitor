7c2003b400e49aa792e9541211bf8c92f454f843
Abdelhafeez <abdelhafeez0nada@gmail.com>
2024-01-17 15:15:17 +0200

    add security components

---
+++ src/main/java/com/basatatech/loggingmonitor/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.basatatech.loggingmonitor;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Configuration
+@Slf4j
+public class SecurityConfig {
+
+    @Value("${security.username}")
+    private String username;
+
+    @Value("${security.password}")
+    private String password;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        log.info("UserDetailsService username: {} , Password: {}", username, password);
+        InMemoryUserDetailsManager inMemoryUserDetailsManager = new InMemoryUserDetailsManager();
+        inMemoryUserDetailsManager.createUser(User.withUsername(username).password(password).build());
+        return inMemoryUserDetailsManager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    // public static void main(String[] args) {
+    // BCryptPasswordEncoder pe = new BCryptPasswordEncoder();
+    // System.out.println(pe.encode("1234"));
+    // }
+}
