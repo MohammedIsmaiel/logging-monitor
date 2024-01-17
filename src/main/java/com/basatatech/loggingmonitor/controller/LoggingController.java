68489bcb6efe228ca9c09e5ba1575cbd987b6400
Abdelhafeez <abdelhafeez0nada@gmail.com>
2024-01-17 14:14:28 +0200

    implement logic for read logs and stop

--- src/main/java/com/basatatech/loggingmonitor/controller/LoggingController.java
+++ src/main/java/com/basatatech/loggingmonitor/controller/LoggingController.java
@@ -1,33 +1,39 @@
 package com.basatatech.loggingmonitor.controller;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.basatatech.loggingmonitor.service.FileTailingService;
+import org.springframework.web.context.WebApplicationContext;
 import com.basatatech.loggingmonitor.service.LoggerService;
-
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @AllArgsConstructor
 @Slf4j
+@Scope(value = WebApplicationContext.SCOPE_SESSION)
 public class LoggingController {
 
-    private final String basedir = "/home/mohamedahmedi/logs/";
     private final SimpMessagingTemplate simpMessagingTemplate;
 
-    /// endpoint to render blank page with button
-
-    @GetMapping("/logs/{name}")
-    public String readLog(@PathVariable String name) {
+    @GetMapping("/logs/{userId}/{name}")
+    public String readLog(@PathVariable String userId, @PathVariable String name) {
         log.info("--------------------{}-------------------", name);
-        String path = new StringBuilder().append(basedir).append(name).append(".log").toString();
-        new LoggerService(path, simpMessagingTemplate).tailFile();
-        // new FileTailingService(path, simpMessagingTemplate).tailFile();
+        LoggerService loggerService = LoggerService.mapUserSession(userId);
+        loggerService.init(name, simpMessagingTemplate, userId);
+        loggerService.tailFile();
+        log.info("Return  readLog()" + name + "-----------------------------");
+        return "done";
+    }
+
+    @GetMapping("/logs/{userId}/{name}/stop")
+    public String stopLog(@PathVariable String userId, @PathVariable String name) {
+        log.info("--------------------{}-------------------", name);
+        LoggerService loggerService = LoggerService.mapUserSession(userId);
+        loggerService.stopWatching();
+        log.info("Return  stopLog()" + name + "-----------------------------");
         return "done";
     }
 
