df93c87730981e54684d4e1dc09d2e3df60bb726
Abdelhafeez <abdelhafeez0nada@gmail.com>
2024-01-17 15:15:02 +0200

    clean

--- src/main/java/com/basatatech/loggingmonitor/LoggingMonitorApplication.java
+++ src/main/java/com/basatatech/loggingmonitor/LoggingMonitorApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LoggingMonitorApplication {
 
-	private static final String INBOUND_PATH = "/connects/logs";
-
 	public static void main(String[] args) {
 
 		SpringApplication.run(LoggingMonitorApplication.class, args);
