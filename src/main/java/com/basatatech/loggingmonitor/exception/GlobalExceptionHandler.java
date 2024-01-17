47f872d90d49ae4c5e7fffcf82c634b4456e5ef8
Mohammed-Ismaiel <mohammedismaiel333.z@gmail.com>
2024-01-17 17:11:37 +0200

    show error page if there is no handler

--- src/main/java/com/basatatech/loggingmonitor/exception/GlobalExceptionHandler.java
+++ src/main/java/com/basatatech/loggingmonitor/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.basatatech.loggingmonitor.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -11,26 +12,26 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ModelAndView handleResourceNotFoundException(ResourceNotFoundException ex) {
-        return createErrorModelAndView("Resource Not Found", ex.getMessage());
+    public String handleResourceNotFoundException(ResourceNotFoundException ex, Model model) {
+        return createErrorModelAndView("Resource Not Found", ex.getMessage(), model);
     }
 
     @ExceptionHandler(NoAvailableLogsException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ModelAndView handleNoAvailableLogsException(NoAvailableLogsException ex) {
-        return createErrorModelAndView("No Available Logs", ex.getMessage());
+    public String handleNoAvailableLogsException(NoAvailableLogsException ex, Model model) {
+        return createErrorModelAndView("No Available Logs", ex.getMessage(), model);
     }
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ModelAndView handleException(Exception ex) {
-        return createErrorModelAndView("Internal Server Error", ex.getMessage());
+    public String handleException(Exception ex, Model model) {
+        return createErrorModelAndView("Internal Server Error", ex.getMessage(), model);
     }
 
-    private ModelAndView createErrorModelAndView(String errorTitle, String errorMessage) {
-        ModelAndView modelAndView = new ModelAndView("error");
-        modelAndView.addObject("errorTitle", errorTitle);
+    private String createErrorModelAndView(String errorTitle, String errorMessage, Model model) {
+        ModelAndView modelAndView = new ModelAndView("redirect:/logs/error");
+        // modelAndView.addObject("errorTitle", errorTitle);
         modelAndView.addObject("errorMessage", errorMessage);
-        return modelAndView;
+        return "error";
     }
 }
