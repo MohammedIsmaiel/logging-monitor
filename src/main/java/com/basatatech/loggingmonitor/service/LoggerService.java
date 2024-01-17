e1aa4517df3d0ebb9bbc4da62b99d6ba6e6762be
Abdelhafeez <abdelhafeez0nada@gmail.com>
2024-01-17 14:17:23 +0200

    implement session handling

--- src/main/java/com/basatatech/loggingmonitor/service/LoggerService.java
+++ src/main/java/com/basatatech/loggingmonitor/service/LoggerService.java
@@ -1,36 +1,89 @@
 package com.basatatech.loggingmonitor.service;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 
-import lombok.AllArgsConstructor;
+import com.basatatech.loggingmonitor.util.LogsFileFilterName;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@AllArgsConstructor
 public class LoggerService {
-
-    private final String filePath;
+    private static final String basedir = "/home/abdelhafeezahmed/logs/";
+    private String filePath;
+    private String topic = "/topic/messages";
     private SimpMessagingTemplate simpMessagingTemplate;
 
+    public static Map<String, LoggerService> USER_MAP = new HashMap<>();
+
+    private boolean watching = false;
+
+    public LoggerService() {
+    }
+
+    public void init(String name, SimpMessagingTemplate simpMessagingTemplate, String userId) {
+        log.info("LoggerService init()");
+        this.simpMessagingTemplate = simpMessagingTemplate;
+        this.filePath = new StringBuilder().append(basedir).append(name).append(".log").toString();
+        this.topic = new StringBuilder().append(this.topic).append("/").append(userId).append("/").append(name)
+                .toString();
+        this.watching = true;
+        System.out.println("TOPIC: " + this.topic);
+    }
+
     public void tailFile() {
-        // Implement your file-tailing logic here
+        log.info("LoggerService tailFile()");
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             String line;
-            while (true) {
+            while (watching) {
                 while ((line = reader.readLine()) != null) {
-                    // Your business logic to process the file content goes here
                     log.info("Received file content: " + line);
-                    simpMessagingTemplate.convertAndSend("/topic/messages", line);
+                    simpMessagingTemplate.convertAndSend(this.topic, line);
                 }
+                line = null;
             }
+            log.info("Exit While *************************");
 
         } catch (IOException e) {
             log.error("Error reading the file: " + e.getMessage());
         }
+    }
+
+    public void stopWatching() {
+        this.watching = false;
+        log.info("-------------- Stop Watching -----------------------");
+    }
+
+    public static LoggerService mapUserSession(String userId) {
+        if (USER_MAP.keySet().contains(userId)) {
+            return USER_MAP.get(userId);
+        }
+        LoggerService newSessionLoggerService = new LoggerService();
+        USER_MAP.put(userId, newSessionLoggerService);
+        return newSessionLoggerService;
+    }
+
+    public static List<String> loadLogsNames() {
+        List<String> logs = new ArrayList<>();
+        File path = new File(basedir);
+        if (!path.exists() || !path.isDirectory())
+            return null;
+        File[] logFiles = path.listFiles(new LogsFileFilterName());
+        String fileName;
+        for (File file : logFiles) {
+            fileName = file.getName();
+            int lastDotIndex = fileName.lastIndexOf(".");
+            logs.add(fileName.substring(0, lastDotIndex));
+        }
+        return logs;
     }
 
 }
