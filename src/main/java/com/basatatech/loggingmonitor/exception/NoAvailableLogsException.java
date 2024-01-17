239b4d51ba047a55abeb3419647acf5f4d2bf316
Abdelhafeez <abdelhafeez0nada@gmail.com>
2024-01-17 16:23:55 +0200

    implement NoAvailableLogsException

---
+++ src/main/java/com/basatatech/loggingmonitor/exception/NoAvailableLogsException.java
@@ -0,0 +1,8 @@
+package com.basatatech.loggingmonitor.exception;
+
+public class NoAvailableLogsException extends RuntimeException {
+
+    public NoAvailableLogsException(String message) {
+        super(message);
+    }
+}
