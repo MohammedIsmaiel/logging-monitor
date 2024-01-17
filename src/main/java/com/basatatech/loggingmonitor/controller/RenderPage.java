30032e24cacac673e5217ffaa4e19f10e19ed8ee
Abdelhafeez <abdelhafeez0nada@gmail.com>
2024-01-17 15:49:20 +0200

    handle logout

--- src/main/java/com/basatatech/loggingmonitor/controller/RenderPage.java
+++ src/main/java/com/basatatech/loggingmonitor/controller/RenderPage.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.basatatech.loggingmonitor.service.LoggerService;
 
@@ -16,13 +17,19 @@ public class RenderPage {
     @GetMapping("/logs")
     public String readLog(Model model) {
         log.info("-- ---------- RenderPage ----------------- --");
-
         // Assume you have a service method that returns a list of log names
         List<String> logNames = getLogNamesFromService();
+        if (logNames == null || logNames.isEmpty()) {
 
+        }
         model.addAttribute("logNames", logNames);
-
         return "logs";
+    }
+
+    @GetMapping("/logs/{userId}/logout")
+    public String getMethodName(@PathVariable String userId) {
+        LoggerService.removeUserSession(userId);
+        return "redirect:/logout";
     }
 
     private List<String> getLogNamesFromService() {
