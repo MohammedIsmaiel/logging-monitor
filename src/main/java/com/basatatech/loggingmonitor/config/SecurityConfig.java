fa69587411dff77b06eac62f92d5488275341cc1
gebali-bee <ahmed.gebali@basatatech.com>
2024-06-03 18:30:42 +0300

    add spring security config for spring admin

--- src/main/java/com/basatatech/loggingmonitor/config/SecurityConfig.java
+++ src/main/java/com/basatatech/loggingmonitor/config/SecurityConfig.java
@@ -1,13 +1,19 @@
 package com.basatatech.loggingmonitor.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -33,4 +39,33 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    @Order(2)
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .authorizeHttpRequests(
+                        authorize -> authorize
+                                .anyRequest().authenticated())
+                .formLogin(Customizer.withDefaults());
+        return http.build();
+
+    }
+
+    @Bean
+    @Order(1)
+    SecurityFilterChain actuatorFilterChain(HttpSecurity http) throws Exception {
+        http
+                .securityMatcher(new AntPathRequestMatcher("/actuator/**"))
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(
+                        authorize -> authorize
+                                .requestMatchers(EndpointRequest.to("health")).permitAll()
+                                .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+
 }
