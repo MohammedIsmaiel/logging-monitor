2a0f891e74515957c772edb5fc8ddbb472136a1e
ahmedalgebali <ahmedelgebalywork@gmail.com>
2024-06-06 21:29:56 +0300

    backup if anything goes wrong

--- src/main/resources/static/js/logs copy.js
+++ src/main/resources/static/js/logs copy.js
@@ -1,19 +1,21 @@
 var client;
 var webSocket = new SockJS('/websocket');
 client = Stomp.over(webSocket);
-
 const submitButton = document.getElementById('submitButton');
 const resetButton = document.getElementById('resetButton');
 const logName = document.getElementById('logName');
 const archiveName = document.getElementById('archiveName');
 var messageList = document.getElementById('messageList');
+var firstLoad = true;  // Flag to track the first load
+var sessionTimeout = 10 * 60 * 1000; // 10 minutes in milliseconds
+var warningTime = 9 * 60 * 1000; // 1 minute before session timeout
+var warningDisplayed = false;
+var activityTimeout;
 
 function generateUserId() {
     return 'user_' + Math.floor(Math.random() * 1000);
 }
 var user_id = generateUserId();
-// var user_id = sessionStorage.getItem('user_id') || generateUserId();
-// sessionStorage.setItem('user_id', user_id);
 
 function logout() {
     var logoutUrl = '/logs/' + user_id + '/logout';
@@ -24,37 +26,86 @@ function loadArchives() {
     var logNameValue = logName.value;
     if (logNameValue !== 'Choose a log to show...') {
         fetch('/logs/' + encodeURIComponent(logNameValue) + '/archives')
-            .then(response => response.json())
-            .then(data => {
-                if (data.length > 0) {
-                    archiveName.style.display = 'block';
-                    archiveName.innerHTML = '<option selected>Choose an archive...</option>';
-                    data.forEach(archive => {
-                        var option = document.createElement('option');
-                        option.value = archive;
-                        option.text = archive;
-                        archiveName.appendChild(option);
-                    });
-                } else {
-                    archiveName.style.display = 'none';
-                }
-            })
-            .catch(error => {
-                console.error('Error fetching archives:', error);
-            });
+        .then(response => response.json())
+        .then(data => {
+            if (data.length > 0) {
+                archiveName.style.display = 'block';
+                archiveName.innerHTML = '<option selected>Choose an archive...</option>';
+                // Sort archive names alphabetically
+                data.sort();
+                data.forEach(archive => {
+                    var option = document.createElement('option');
+                    option.value = archive;
+                    option.text = archive;
+                    archiveName.appendChild(option);
+                });
+            } else {
+                archiveName.style.display = 'none';
+            }
+        })
+        .catch(error => {
+            console.error('Error fetching archives:', error);
+        });
     } else {
         archiveName.style.display = 'none';
     }
 }
 
+function resetActivityTimeout() {
+    clearTimeout(activityTimeout);
+    activityTimeout = setTimeout(showSessionWarning, warningTime);
+}
+
+function showSessionWarning() {
+    warningDisplayed = true;
+    var warningMessage = "Your session is about to expire. Click OK to keep it alive.";
+    if (confirm(warningMessage)) {
+        sendHeartbeat(); // Send a heartbeat to the server to keep the session alive
+        resetActivityTimeout(); // Reset the activity timeout
+        warningDisplayed = false;
+    } 
+}
+
+document.addEventListener('mousemove', resetActivityTimeout);
+document.addEventListener('keydown', resetActivityTimeout);
+resetActivityTimeout(); // Initial call to set the timeout
+
+function sendHeartbeat() {
+    fetch('/logs/' + encodeURIComponent(user_id) + '/heartbeat', { method: 'GET' })
+        .then(response => {
+            if (response.ok) {
+                console.log('Heartbeat sent successfully');
+            } else {
+                console.error('Failed to send heartbeat');
+            }
+        })
+        .catch(error => {
+            console.error('Error sending heartbeat:', error);
+        });
+}
+setInterval(sendHeartbeat, 5 * 60 * 1000); // Send heartbeat every 5 minutes
+
+// close the session in refreshing 
+window.addEventListener('beforeunload', function () {
+    fetch('/logs/' + encodeURIComponent(user_id) + '/logout', { method: 'GET' });
+});
+function autoScroll() {
+    messageList.scrollTop = messageList.scrollHeight;
+}
+
+function markAsRead() {
+    const unreadMessages = messageList.querySelectorAll('.unread');
+    unreadMessages.forEach(message => {
+        message.classList.remove('unread');
+    });
+}
+function checkScroll() {
+    if (messageList.scrollTop + messageList.clientHeight >= messageList.scrollHeight) {
+        markAsRead();
+    }
+}
 client.connect({}, function (frame) {
     console.log('Connected: ' + frame);
-
-    // // Check if there's a log being watched and re-establish the connection if needed
-    // var watchedLog = sessionStorage.getItem('watchedLog');
-    // if (watchedLog) {
-    //     resumeWatchingLog(watchedLog);
-    // }
 
     document.getElementById('logForm').addEventListener('submit', function (event) {
         event.preventDefault();
@@ -75,17 +126,17 @@ client.connect({}, function (frame) {
             url += encodeURIComponent(logNameValue) + '?archive=false';
         }
 
-        // // Save the current log being watched in session storage
-        // sessionStorage.setItem('watchedLog', logNameValue);
-        // sessionStorage.setItem('isArchive', isArchive);
-
         if (!isArchive) {
             client.subscribe(topicPath, function (message) {
                 console.log(message.body);
                 var item = document.createElement('p');
                 item.appendChild(document.createTextNode(message.body));
-                item.classList.add('m-0');
+                item.classList.add('m-0', 'unread');
                 messageList.appendChild(item);
+                if (firstLoad) {  // Auto-scroll only on the first load
+                    autoScroll();
+                    firstLoad = false;  // Set the flag to false after the first load
+                }
             });
         }
 
@@ -112,50 +163,6 @@ client.connect({}, function (frame) {
     });
 });
 
-// function resumeWatchingLog(logName) {
-//     var isArchive = sessionStorage.getItem('isArchive') === 'true';
-//     var topicPath = '/topic/messages/' + encodeURIComponent(user_id) + '/';
-//     var url = '/logs/' + encodeURIComponent(user_id) + '/';
-//     if (isArchive) {
-//         topicPath += encodeURIComponent(logName);
-//         url += encodeURIComponent(logName) + '?archive=true';
-//     } else {
-//         topicPath += encodeURIComponent(logName);
-//         url += encodeURIComponent(logName) + '?archive=false';
-//     }
-
-//     if (!isArchive) {
-//         client.subscribe(topicPath, function (message) {
-//             console.log(message.body);
-//             var item = document.createElement('p');
-//             item.appendChild(document.createTextNode(message.body));
-//             item.classList.add('m-0');
-//             messageList.appendChild(item);
-//         });
-//     }
-
-//     fetch(url)
-//         .then(response => {
-//             if (!response.ok) {
-//                 throw new Error('Network response was not ok');
-//             }
-//             return response.text();
-//         })
-//         .then(data => {
-//             console.log('Response:', data);
-//             if (isArchive) {
-//                 var item = document.createElement('pre');
-//                 item.appendChild(document.createTextNode(data));
-//                 item.classList.add('m-0');
-//                 messageList.innerHTML = ''; // Clear previous content
-//                 messageList.appendChild(item);
-//             }
-//         })
-//         .catch(error => {
-//             console.error('Error:', error);
-//         });
-// }
-
 resetButton.addEventListener('click', function (event) {
     event.preventDefault();
     var logNameValue = document.getElementById('logName').value;
@@ -176,8 +183,5 @@ resetButton.addEventListener('click', function (event) {
             console.error('Error stopping log:', error);
         });
 
-    // Clear session storage and reload
-    // sessionStorage.removeItem('watchedLog');
-    // sessionStorage.removeItem('isArchive');
     location.reload(true);
 });
